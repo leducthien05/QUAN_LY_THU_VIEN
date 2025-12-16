5cd6d2f38a32f4959b514f0d977c411ffa915f28
DinhThanh-0792 <danhconmeo225@gmail.com>
2025-12-16 08:40:27 +0700

    Add files via upload

---
+++ DAL/Connect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class Connect
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=.\SQLEXPRESS;
+                                                 Initial Catalog=QUAN_LY_THU_VIEN;
+                                                 Integrated Security=True");
+        SqlCommand cmd;
+        public DataTable LoadData(string sql)
+        {
+            using (SqlDataAdapter da = new SqlDataAdapter(sql, conn))
+            {
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+        }
+        public void ExecuteNonQuery(SqlCommand cmd)
+        {
+            cmd.Connection = conn;
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
---
+++ DAL/TacGia_DAL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using DTO;
+
+namespace DAL
+{
+    public class TacGia_DAL: Connect
+    {
+        public DataTable LoadTacGia()
+        {
+            string sql = "SELECT * FROM TACGIA";
+            return LoadData(sql);
+        }
+        public void InsertTacGia(TacGia tg)
+        {
+            SqlCommand cmd = new SqlCommand("INSERT INTO TACGIA (ID_TG, TenTG, QuocGia) VALUES (@ID_TG, @TenTG, @QuocGia)");
+            cmd.Parameters.AddWithValue("@ID_TG" +
+                "", tg.MaTG);
+            cmd.Parameters.AddWithValue("@TenTG", tg.TenTG);
+            cmd.Parameters.AddWithValue("@QuocGia", tg.QuocGia);
+            ExecuteNonQuery(cmd);
+        }
+        public void UpdateTacGia(TacGia tg)
+        {
+            SqlCommand cmd = new SqlCommand("UPDATE TACGIA SET TenTG = @TenTG, QuocGia = @QuocGia WHERE MaTG = @MaTG");
+            cmd.Parameters.AddWithValue("@MaTG", tg.MaTG);
+            cmd.Parameters.AddWithValue("@TenTG", tg.TenTG);
+            cmd.Parameters.AddWithValue("@QuocGia", tg.QuocGia);
+            ExecuteNonQuery(cmd);
+        }
+        public void DeleteTacGia(string maTG)
+        {
+            SqlCommand cmd = new SqlCommand("DELETE FROM TACGIA WHERE MaTG = @MaTG");
+            cmd.Parameters.AddWithValue("@MaTG", maTG);
+            ExecuteNonQuery(cmd);
+        }
+    }
+}
