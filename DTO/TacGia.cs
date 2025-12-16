404381c24d119728ec436fff05ae7ab15234716e
DinhThanh-0792 <danhconmeo225@gmail.com>
2025-12-16 08:40:56 +0700

    Add files via upload

---
+++ DTO/TacGia.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class TacGia
+    {
+        public string MaTG { get; set; }
+        public string TenTG { get; set; }
+        public string QuocGia { get; set; }
+        public TacGia() { }
+        public TacGia(string maTG, string tenTG, string quocGia)
+        {
+            this.MaTG = maTG;
+            this.TenTG = tenTG;
+            this.QuocGia = quocGia;
+        }
+    }
+}
